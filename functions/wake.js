5e055139c315ef4cb5e78ae4cb17682e2406791d
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-03-12 16:47:56 -0700

    post method working correctly

--- functions/wake.js
+++ functions/wake.js
@@ -1,6 +1,22 @@
+const wol = require('wol');
+
 exports.handler = function(event, context, callback) {
+    if (event.httpMethod !== "POST") {
+        callback(new Error("Invalid HTTP request"));
+        return;
+    }
+
+    const data = JSON.parse(event.body);
+
+    if (!data.macAddress) {
+        callback(new Error("MAC address missing"));
+        return;
+    }
+
+    wol.wake(data.macAddress);
+
     callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
+        statusCode: 200,
+        body: "Success"
     });
 }
