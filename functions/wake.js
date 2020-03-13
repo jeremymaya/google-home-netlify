f00075f374e146ac53299eebc25e1ac6d72e13d7
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-03-12 21:54:38 -0700

    testing different wol module

--- functions/wake.js
+++ functions/wake.js
@@ -1,28 +1,21 @@
-const axios = require('axios')
-const wol = require('wol')
+const axios = require('axios');
+const wol = require('wake_on_lan');
 
 exports.handler = function(event, context, callback) {
-    const data = JSON.parse(event.body)
-
-    console.log(data)
-    console.log(data.macAddress)
-    wol.wake(data.macAddress)
+    const data = JSON.parse(event.body);
 
     axios({
         method: 'post',
         url: 'http://requestbin.net/r/19wesms1',
         data: data.macAddress
     }).then(response => {
-        console.log(response.config.data)
-        wol.wake(response.config.data, function(err, res){
-            console.log(res);
-          });
+        wol.wake(response.config.data)
         callback(null, {
             statusCode: 200,
             body: 'Success'
-        })
+        });
     }).catch(err =>{
         console.log(err)
         callback(new Error('Failed'))
-    })
+    });
 }
