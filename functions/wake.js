1a60ddf338c8c73924df3430d9ac493e0b31db97
Kyungrae Kim <jeremiah.kim@hotmail.com>
2020-03-19 22:57:54 -0700

    reverted back to using wol npm module before trying out JAM stack route

--- functions/wake.js
+++ functions/wake.js
@@ -1,22 +1,21 @@
 const axios = require('axios');
-const wol = require('wakeonlan');
+const wol = require('wol');
 
 exports.handler = function(event, context, callback) {
     const data = JSON.parse(event.body);
 
     axios({
         method: 'post',
-        url: 'http://requestbin.net/r/1ax4yog1',
         data: data.macAddress
     }).then(response => {
-        console.log(response.config.data)
+        console.log(response.config.data);
         wol.send(response.config.data);
         callback(null, {
             statusCode: 200,
             body: 'Success'
         });
     }).catch(err =>{
-        console.log(err)
-        callback(new Error('Failed'))
+        console.log(err);
+        callback(new Error('Failed'));
     });
 }
